aad3a51c7bdcbcdf14b2b7b85bafe11e1b86e9ca
Riley Moss <riley.moss@accenture.com>
2021-01-06 10:02:33 -0600

    Added Routes and Basic Pages

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+
+const Home = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Home</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+        </IonPage>
+    )
+};
+
+export default Home;
